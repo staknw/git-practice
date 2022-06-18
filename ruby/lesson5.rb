3b07f3520a90f029047c1542d2fd42d74cbe9194
staknw <107232262+staknw@users.noreply.github.com>
2022-06-18 13:15:09 +0000

    rubytest

---
+++ ruby/lesson5.rb
@@ -0,0 +1,30 @@
+total_price = 100
+
+if total_price > 100 
+  puts "みかんを購入。所持金に余りあり"
+  elsif total_price == 100
+    puts "みかんを購入。所持金は0円"
+  else
+    puts "みかんを購入することができません"
+end
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+#total_price = 100
+#if total_price < 200
+#  puts "みかんを購入。所持金に余りあり"
+#elsif == 100
+#  puts "みかんを購入。所持金は0円"
+#else > 100 
+# puts "みかんを購入することができません"
+#end
