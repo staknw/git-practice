b0420b7467165c686e332457467080d2f72b4c77
staknw <107232262+staknw@users.noreply.github.com>
2022-06-19 08:36:30 +0000

    ruby練習

---
+++ ruby/greeting.rb
@@ -0,0 +1,15 @@
+def greeting(name)
+  return "Hello, #{name}!"
+  "Good morning, #{name}!"
+end
+
+puts greeting('John')
+
+
+
+#def greeting(name)
+  #retum "Hello,#{name}!"
+  #"Good moming,#{name}!"
+#end
+
+#puts greeting('john')
