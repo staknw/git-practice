6ef89b60278ac0b108571e869265df87a6cdb0e1
staknw <107232262+staknw@users.noreply.github.com>
2022-06-19 03:43:23 +0000

    ruby練習

---
+++ ruby/lesson6-1.rb
@@ -0,0 +1,22 @@
+#lesson = "計算をはじめます"
+# lesson
+#lesson6 = "２つの値を入力してください"
+#puts lesson6
+
+#a = gets
+#b = gets
+#puts "計算結果を出力します"
+
+#puts 4 * 5 = 20
+#puts "計算を終了します"
+
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+puts "計算結果を出力します"
+puts "a*b=#{a*b}"
+puts "計算を終了します"
+
